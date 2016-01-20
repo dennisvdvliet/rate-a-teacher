4d23b9e6324486c828012404d94449c1636c2f61
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-01-20 19:02:03 +0100

    Adding scaffold for votes
    Votes are the model we use to store votes made by students. The first
    implementation will only support anonymous votes and not votes related
    to students. This could be a nice pull request

---
+++ app/views/votes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @vote, :id, :stars, :lecture_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :votes
   resources :lectures do
     member do
       get :vote
