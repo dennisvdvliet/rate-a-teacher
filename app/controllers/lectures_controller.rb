31cc1a66d5d06ec7ec484c73190b7c994f00f324
Dennis van der Vliet <dennis.vandervliet@gmail.com>
2016-02-11 10:41:53 +0100

    Prevent hacking
    How could this be abused by somebody trying to game the system?

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -75,6 +75,6 @@ class LecturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def lecture_params
-      params.require(:lecture).permit(:name, :date, :teacher_id)
+      params.require(:lecture).permit(:name, :date)
     end
 end
